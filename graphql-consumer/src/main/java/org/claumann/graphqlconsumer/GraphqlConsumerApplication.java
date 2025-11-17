07708015213d35490d1a00c09b910708d3de5344
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-11-17 20:41:13 -0300

    feat: project to consume http graphql

---
+++ graphql-consumer/src/main/java/org/claumann/graphqlconsumer/GraphqlConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.claumann.graphqlconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlConsumerApplication.class, args);
+    }
+
+}
